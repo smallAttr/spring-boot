e8c6ced82c01f7d60c9c2223093f67d88bc25e92
chengang <chengang@xingshulin.com>
2018-09-21 17:00:59 +0800

    paramType = query

--- spring-swagger2/src/main/java/com/smallAttr/github/controller/UserController.java
+++ spring-swagger2/src/main/java/com/smallAttr/github/controller/UserController.java
@@ -4,12 +4,10 @@ import com.smallAttr.github.domain.User;
 import com.smallAttr.github.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Author: chenGang
@@ -31,5 +29,15 @@ public class UserController {
   public User findById(@PathVariable("id") Long id) {
     return userService.findById(id);
   }
+
+
+  @ApiOperation(value = "根据userId查询用户信息", notes = "根据userId查询用户信息")
+  @ApiImplicitParam(name = "id", value = "用户ID", paramType = "query", required = true, dataType = "Long")
+  @GetMapping("")
+  public User getUser(@RequestParam(name = "id") Long id) {
+    return userService.findById(id);
+  }
+
+
 
 }
